765f7dbc6df44aa7fb33718c202ecbf5797eab12
bhushan <muthiyanbhushan@gmail.com>
2017-03-18 16:28:08 -0700

    Rosbag files mapped for SJSU Engineering Building 2nd floor

---
+++ ROSBAG.py
@@ -0,0 +1,12 @@
+import rosbag
+bag = rosbag.Bag('test.bag')
+for topic, msg, t in bag.read_messages(topics=['chatter', 'numbers']):
+    pub.publish(msg)
+bag.close()
+
+if __name__ == '__main__':
+    print("Laser node started")
+    rospy.init_node('ros_bag',anonymous = True)
+    rospy.Subscriber("scan",LaserScan,callback)
+
+    rospy.spin()
